8c85c1ec25a254ef0176d0b00390308c90cc9925
Matvei Kotenev <kotenev.matvei@gmail.com>
2025-06-22 22:49:38 +0200

    take the old lsp config

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,54 +1,77 @@
 return {
   {
     "williamboman/mason.nvim",
-    lazy = false,
     config = function()
       require("mason").setup()
     end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "clangd", "texlab", "ruff", "jedi_language_server", "cssls" },
+        ensure_installed = { "lua_ls", "pyright", "clangd", "texlab", "ruff", "jedi_language_server", "cssls"},
       })
     end,
   },
   {
     "neovim/nvim-lspconfig",
-    lazy = false,
     config = function()
-      local cmp_nvim_lsp = require("cmp_nvim_lsp")
-      local capabilities = vim.tbl_deep_extend(
-        "force",
-        {},
-        vim.lsp.protocol.make_client_capabilities(),
-        cmp_nvim_lsp.default_capabilities()
-      )
-
       local lspconfig = require("lspconfig")
-
-      lspconfig.tailwindcss.setup({
-        capabilities = capabilities
-      })
-      lspconfig.ruby_lsp.setup({
+      lspconfig.cssls.setup({})
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.pyright.setup({
+        settings = {
+          -- pyright = {
+          --   disableOrganizeImports = true,
+          -- },
+          python = {
+            analysis = {
+              ignore = { "*" },
+              autoSearchPaths = true,
+            },
+          },
+        },
         capabilities = capabilities,
-        cmd = { "/home/typecraft/.asdf/shims/ruby-lsp" }
       })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
+      lspconfig.jedi_language_server.setup({
+        on_attach = function(client)
+          client.server_capabilities.hoverProvider = true
+          client.server_capabilities.renameProvider = false
+        end,
+        capabilities = capabilities,
+      })
+      lspconfig.clangd.setup({
+        -- args = {
+        --   "-I/usr/lib/x86_64-linux-gnu/mpich/include",
+        --   "-L/usr/lib/x86_64-linux-gnu/mpich/lib",
+        --   "-lmpi"
+        -- },
+        capabilities = capabilities,
+      })
+      lspconfig.ruff.setup({
+        capabilities = capabilities,
+      })
+      -- Disable hover in favor of Pyright
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client == nil then
+            return
+          end
+          if client.name == "ruff" then
+            client.server_capabilities.hoverProvider = false
+          end
+        end,
+        desc = "LSP: Disable hover capability from Ruff",
       })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<leader>do", vim.lsp.buf.document_symbol, {})
+      vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<leader>i", vim.lsp.buf.implementation, {})
     end,
   },
 }
